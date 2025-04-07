e1081b5fd021b2e6a26ce228702929bd07e1601a
lamgaopc <hophamlam92@yahoo.com>
2025-04-07 16:24:13 +0700

    change to edit table

--- app/protected/edit-table/page.tsx
+++ app/protected/edit-table/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from "@/utils/supabase/server";
 
 export default async function Page() {
   const supabase = await createClient();
-  const { data: notes } = await supabase.from("notes").select();
+  const { data: notes } = await supabase.from("haolamproject_edit").select();
 
   return <pre>{JSON.stringify(notes, null, 2)}</pre>;
 }
